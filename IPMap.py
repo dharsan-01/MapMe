e293bf2cdc8b3a955934914beb25ead89e77247a
Dharsan <100560681+dharsan-01@users.noreply.github.com>
2023-06-24 19:10:44 +0530

    Update IPMap.py

--- IPMap.py
+++ IPMap.py
@@ -20,25 +20,20 @@ def generate_map():
         status_label.config(text="Invalid IP address")
 
 
-# Create the main window
 window = tk.Tk()
 window.title("IP Geolocation Map")
 window.geometry("400x200")
 
-# Create a label and an entry field for IP address input
 label = tk.Label(window, text="Enter IPv4 address:")
 label.pack()
 
 entry = tk.Entry(window)
 entry.pack()
 
-# Create a button to generate the map
 button = tk.Button(window, text="Generate Map", command=generate_map)
 button.pack()
 
-# Create a label to display the status
 status_label = tk.Label(window, text="")
 status_label.pack()
 
-# Start the main loop
 window.mainloop()
